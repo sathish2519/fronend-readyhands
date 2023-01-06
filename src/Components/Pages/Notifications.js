557d4b1691ffac98dbdbd29b6d2845b192825dd0
Sathish -Official <sathishkumar25800@gmail.com>
2023-01-06 19:35:07 +0530

    added mark all as seen and delete all logic

--- src/Components/Pages/Notifications.js
+++ src/Components/Pages/Notifications.js
@@ -1,21 +1,71 @@
 import { Tabs } from 'antd'
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import Layout from '../Layout/Layout'
 import { Card } from 'antd'
 import "./Applyserviceprovider.css"
+import { toast } from 'react-hot-toast'
+import axios from 'axios'
+import { setUser } from '../../Redux/userSlice'
 
 function Notifications() {
     const { user } = useSelector((state) => state.user)
     const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+
+    const MarkAllasSeen = async () => {
+        try {
+            const response = await axios.post("/api/user/mark-all-as-seen", { userId: user._id }, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+            },)
+            if (response.data.success) {
+                toast.success(response.data.message)
+                dispatch(setUser(response.data.data))
+            }
+            else {
+                toast.error(response.data.message)
+            }
+
+        } catch (error) {
+            toast.error("Something went wrong")
+
+        }
+
+    }
+    const deleteall = async () => {
+        try {
+            const response = await axios.post("/api/user/delete-all", { userId: user._id }, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+            },)
+            if (response.data.success) {
+                toast.success(response.data.message)
+                dispatch(setUser(response.data.data))
+            }
+            else {
+                toast.error(response.data.message)
+            }
+
+        } catch (error) {
+            toast.error("Something went wrong")
+
+        }
+
+    }
+
+
     return (
         <Layout>
             <h1 className="page-title">Notifications</h1>
             <Tabs>
                 <Tabs.TabPane tab="Unseen" key={0}>
-                    <div className="d-flex justify-content-end">
-                        <h1 className="anchor">Mark all as seen</h1>
+                    <div className="d-flex justify-content-end" >
+                        <h1 onClick={MarkAllasSeen} className="anchor">Mark all as seen</h1>
                     </div>
                     {user?.UnseenNotification.map((notification) => (<div className='p-2' onClick={() => navigate(notification.onClickPath)}>
                         <Card className='notification-card'
@@ -25,14 +75,25 @@ function Notifications() {
                             }}
                         >
                             <p>{notification.message}</p>
-    
+
                         </Card>
                     </div>))}
                 </Tabs.TabPane>
                 <Tabs.TabPane tab="Seen" key={1}>
                     <div className="d-flex justify-content-end">
-                        <h1 className="anchor">Delete all</h1>
+                        <h1 className="anchor" onClick={deleteall} >Delete all</h1>
                     </div>
+                    {user?.SeenNotification.map((notification) => (<div className='p-2' onClick={() => navigate(notification.onClickPath)}>
+                        <Card className='notification-card'
+                            style={{
+                                border: '1px solid black',
+
+                            }}
+                        >
+                            <p>{notification.message}</p>
+
+                        </Card>
+                    </div>))}
                 </Tabs.TabPane>
 
             </Tabs>
